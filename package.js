08aac9d1fafd8e647ecc7a4fb8dd70b125aed05d
Michiel ter Reehorst <michiel@swydo.com>
2017-03-25 13:04:49 +0100

    0.0.2

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ var packages = [
 
 Package.describe({
   name: 'swydo:graphql',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Compiler plugin that supports GraphQL files in Meteor',
   git: 'https://github.com/swydo/meteor-graphql',
   documentation: 'README.md',
