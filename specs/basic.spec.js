3ebd229da2f422043fd4866a9d8057f9adef8dd3
Michiel ter Reehorst <michiel@swydo.com>
2017-04-28 10:58:06 +0200

    Move examples to own directory

--- specs/basic.spec.js
+++ specs/basic.spec.js
@@ -4,25 +4,25 @@ import { chai } from 'meteor/practicalmeteor:chai';
 
 describe('graphql import', function () {
   it('should import .graphql', function () {
-    const { kind, definitions } = require('./example.graphql');
+    const { kind, definitions } = require('./examples/example.graphql');
     chai.expect(kind).to.be.ok;
     chai.expect(definitions).to.be.ok;
   });
 
   it('should import .graphqls', function () {
-    const { kind, definitions } = require('./schema.graphqls');
+    const { kind, definitions } = require('./examples/schema.graphqls');
     chai.expect(kind).to.be.ok;
     chai.expect(definitions).to.be.ok;
   });
 
   it('should import .gql', function () {
-    const { kind, definitions } = require('./example2.gql');
+    const { kind, definitions } = require('./examples/example2.gql');
     chai.expect(kind).to.be.ok;
     chai.expect(definitions).to.be.ok;
   });
 
   it('should import empty .graphql', function () {
-    const empty = require('./empty.graphql');
+    const empty = require('./examples/empty.graphql');
 
     chai.expect(empty).to.deep.equal({});
   });
