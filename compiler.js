d0edf7cffa012b7f7c8144379a7da0a08c57eedb
Michiel ter Reehorst <michiel@swydo.com>
2017-04-28 10:33:48 +0200

    Cleanup compiler by using more Ecmascript features

--- compiler.js
+++ compiler.js
@@ -1,24 +1,27 @@
-/* eslint-disable prefer-arrow-callback, func-names */
+/* eslint-disable prefer-arrow-callback, func-names, class-methods-use-this */
 import loader from 'graphql-tag/loader';
 
-function GraphQLCompiler() {}
-GraphQLCompiler.prototype.processFilesForTarget = function (files) {
-  files.forEach(function (file) {
-    // Fake webpack context
-    // See https://github.com/apollographql/graphql-tag/blob/master/loader.js#L26
+export default class GraphQLCompiler {
+  processFilesForTarget(files) {
+        // Fake webpack context
+        // See https://github.com/apollographql/graphql-tag/blob/master/loader.js#L26
     const context = {
       cacheable() {},
     };
 
-    const content = file.getContentsAsString().trim();
-    let output = '';
+    files.forEach(function (file) {
+      const path = `${file.getPathInPackage()}.js`;
+      const content = file.getContentsAsString().trim();
+      let data = '';
 
-    if (content) {
-      output = loader.call(context, file.getContentsAsString());
-    }
+      if (content) {
+        data = loader.call(context, content);
+      }
 
-    file.addJavaScript({ data: output, path: `${file.getPathInPackage()}.js` });
-  });
-};
-
-export default GraphQLCompiler;
+      file.addJavaScript({
+        data,
+        path,
+      });
+    });
+  }
+}
