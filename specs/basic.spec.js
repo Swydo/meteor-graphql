5730e3d26f6080933e4e1612873e55fb0f6095ec
Michiel ter Reehorst <michiel@swydo.com>
2017-06-16 21:27:14 +0200

    Add descriptor tests

--- specs/basic.spec.js
+++ specs/basic.spec.js
@@ -33,4 +33,12 @@ describe('graphql import', function () {
     chai.expect(kind).to.be.ok;
     chai.expect(definitions.length).to.equal(2);
   });
+
+  it('should handle comments as descriptors', function () {
+    const { definitions } = require('./examples/comments.graphql');
+
+    chai.expect(definitions[0].description).to.equal('This is a type descriptor');
+    chai.expect(definitions[0].fields[0].description).to.equal('This is a field descriptor');
+    chai.expect(definitions[0].fields[1].description).to.equal(undefined);
+  });
 });
